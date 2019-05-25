c7b346f86a228a9de4acaff1c5ffe738595462ea
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-05-25 23:58:21 +0800

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/javascript">
+  // @ is an alias to /src
+import Navigation from '@/components/Navigation.vue'
 
 export default {
-  name: 'app',
+  name: 'home',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style type="text/css">
+  #app {
+    text-align: center;
+  }
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import 'bootstrap-select'
+// import 'core-js'
+// import 'jquery'
+// import 'leaflet'
+// import 'magnific-popup'
+// import 'nouislider'
+// import 'object-fit-images'
+// import 'popper.js'
+// import 'prismjs'
+// import 'smooth-scroll'
+// import 'swiper'
+
+// import Bootstrap from 'bootstrap'
+// import BootstrapSelect from 'bootstrap-select'
+// import CoreJS from 'core-js'
+// import jQuery from 'jquery'
+// import Leaflet from 'leaflet'
+// import Magnific from 'magnific-popup'
+// import Nouslider from 'nouislider'
+// import ObjectFit from 'object-fit-images'
+// import Popper from 'popper.js'
+// import Prism from 'prismjs'
+// import SmoothScroll from 'smooth-scroll'
+// import Swiper from 'swiper'
+
+// Vue.use(Bootstrap)
+// Vue.use(BootstrapSelect)
+// Vue.use(CoreJS)
+// Vue.use(jQuery)
+// Vue.use(Leaflet)
+// Vue.use(Magnific)
+// Vue.use(Nouslider)
+// Vue.use(ObjectFit)
+// Vue.use(Prism)
+// Vue.use(SmoothScroll)
+// Vue.use(Swiper)
+
+
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
