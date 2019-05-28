abd69dfbbda8d04fc086bd9787cfc8d6496e7c01
Eddrich Janzzen Ang <eang.ayannah@gmail.com>
2019-05-28 14:35:21 +0800

    Add cdn for css in App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,11 @@ export default {
     text-align: center;
   }
 
-
+  /*extra css*/
+  @import "https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700";
+  @import "https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700";
+  @import "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css";
+  @import "https://use.fontawesome.com/releases/v5.8.1/css/all.css";
 
 </style>
+
