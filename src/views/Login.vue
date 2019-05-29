55b39c264466f3c1768f83fc245860817d5eecb7
Eddrich Janzzen Ang <eang.ayannah@gmail.com>
2019-05-29 15:53:28 +0800

    Fix rendering for assets

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,9 +4,17 @@
           <div class="row min-vh-100">
             <div class="col-md-8 col-lg-6 col-xl-5 d-flex align-items-center">
               <div class="w-100 py-5 px-md-5 px-xl-6 position-relative">
-                <div class="mb-5"><img src="img/logo-square.svg" alt="..." style="max-width: 4rem;" class="img-fluid mb-3">
+                <div class="mb-5"><img src="../assets/img/logo-square.svg" alt="..." style="max-width: 4rem;" class="img-fluid mb-3">
                   <h2>Welcome back</h2>
                 </div>
+                <a href="/" class="close-absolute mr-md-5 mr-xl-6 pt-5"> 
+                  <svg class="svg-icon w-3rem h-3rem">
+                    <use xlink:href="#close-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="close-1" width="100%" height="100%">
+                      <path data-name="layer1" fill="none" stroke="#202020" stroke-miterlimit="10" d="M41.999 20.002l-22 22m22 0L20 20" stroke-linejoin="round" stroke-linecap="round" style="stroke:var(--layer1, #202020)"></path>
+                    </svg>
+                    </use>
+                  </svg></a>
                 <form class="form-validate">
                   <div class="form-group">
                     <label for="loginUsername" class="form-label"> Email Address</label>
@@ -34,15 +42,12 @@
                   <button class="btn btn btn-outline-muted btn-block btn-social mb-3"><i class="fa-2x fa-google fab btn-social-icon"> </i>Connect <span class="d-none d-sm-inline">with Google</span></button>
                   <hr class="my-4">
                   <p class="text-center"><small class="text-muted text-center">Don't have an account yet? <a href="signup.html">Sign Up                </a></small></p>
-                </form><a href="index.html" class="close-absolute mr-md-5 mr-xl-6 pt-5"> 
-                  <svg class="svg-icon w-3rem h-3rem">
-                    <use xlink:href="#close-1"> </use>
-                  </svg></a>
+                </form>
               </div>
             </div>
-            <div class="col-md-4 col-lg-6 col-xl-7 d-none d-md-block">
+            <div class="col-md-4 col-lg-6 col-xl-7 bG">
               <!-- Image-->
-              <div style="background-image: url(img/photo/photo-1497436072909-60f360e1d4b1.jpg);" class="bg-cover h-100 mr-n3"></div>
+              
             </div>
           </div>
         </div>
@@ -59,7 +64,10 @@ export default {
 }
 </script>
 
-<style type="text/css">
-    
+<style>
+    .bG {
+      background: url(../assets/img/photo/photo-1497436072909-60f360e1d4b1.jpg) center center;
+      background-size: cover;
+    }
     
 </style>
