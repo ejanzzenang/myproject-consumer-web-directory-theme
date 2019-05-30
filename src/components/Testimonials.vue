5ee5c4c915579d2154fed770fd414e55a417e389
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-05-30 22:37:50 +0800

    Add about display1, display2, testimonial to Home.vue

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="testimonials">
+		    <section class="py-7">
+      <div class="container">
+        <div class="text-center">
+          <p class="subtitle text-primary">Testimonials</p>
+          <h2 class="mb-5">Our dear customers said about us</h2>
+        </div>
+        <!-- Slider main container-->
+        <div class="swiper-container testimonials-slider testimonials">
+          <!-- Additional required wrapper-->
+          <div class="swiper-wrapper pt-2 pb-5">
+            <!-- Slides-->
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide px-3">
+              <div class="testimonial card rounded-lg shadow border-0">
+                <div class="testimonial-avatar"><img src="../assets/img/avatar/avatar-3.jpg" alt="..." class="img-fluid"></div>
+                <div class="text">
+                  <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                  <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p><strong>Jessica Watson</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </section>
+
+
+	</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-
     <SearchBar/>
+    <AboutProduct/>
+    <DisplayCatalog1/>
+    <DisplayCatalog2/>
+    <Testimonials/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SearchBar from '@/components/SearchBar.vue'
+import AboutProduct from '@/components/AboutProduct.vue'
+import DisplayCatalog1 from '@/components/DisplayCatalog1.vue'
+import DisplayCatalog2 from '@/components/DisplayCatalog2.vue'
+import Testimonials from '@/components/Testimonials.vue'
+
 
 export default {
   name: 'home',
   components: {
-    SearchBar
+  	SearchBar, 
+  	AboutProduct,
+  	DisplayCatalog1,
+  	DisplayCatalog2,
+  	Testimonials
   }
 }
 
