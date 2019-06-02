f79859b35fe092e3c7648563510dede2771dbfb3
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-06-02 21:18:31 +0800

    Add products index component

--- src/components/products/index.vue
+++ src/components/products/index.vue
@@ -1,7 +1,29 @@
 <template>
 	<div class="products">
-		
-
-
+		<h1>Product Catalog</h1>
+		<DisplayCatalog2/>
+		<DisplayCatalog3/>		
 	</div>
 </template>
+
+<style scoped>
+	h1 {
+		margin-top: 100px;
+		text-align: center;
+	}	
+</style>
+
+<script type="text/javascript">
+	import DisplayCatalog2 from '@/components/DisplayCatalog2.vue'
+	import DisplayCatalog3 from '@/components/DisplayCatalog3.vue'
+
+
+export default {
+name: 'products',
+	components: {
+		DisplayCatalog2,
+		DisplayCatalog3
+	}
+}
+
+</script>
