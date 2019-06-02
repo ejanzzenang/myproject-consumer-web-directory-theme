c286d9c67a4ba7b314d8611f6655f26b88e68633
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-06-02 21:18:08 +0800

    Add product index

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,6 +1,26 @@
 <template>
 	<div class="product">
+		<Products/>
 		<router-view/>	
 	</div>
 </template>
+<script>
+	
+// @ is an alias to /src
+import Products from '@/components/products'
 
+
+export default {
+name: 'products',
+	components: {
+		Products
+	}
+}
+
+</script>
+
+<style scoped>
+	
+
+
+</style>
