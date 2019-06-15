eaccd5dfae193aeaedee5540e31dbc9d4d536f77
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-06-15 21:41:48 +0800

    Add logout button

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -19,10 +19,28 @@
               <div aria-labelledby="homeDropdownMenuLink" class="dropdown-menu"><a href="index.html" class="dropdown-item">Rooms</a><a href="index-2.html" class="dropdown-item">Restaurants</a></div>
             </li>
             </li>
-            <li class="nav-item"><a href="/login" class="nav-link">Sign in</a></li>
+            <li class="nav-item"><a id="logInButton" href="/login" class="nav-link">Sign in</a></li>
             <li class="nav-item"><a href="/signup" class="nav-link">Sign up</a></li>
+            <li class="nav-item mt-3 mt-lg-0 ml-lg-3 d-lg-none d-xl-inline-block"><button id="logOutButton" class="btn btn-primary">Logout</button></li>
           </ul>
         </div>
       </div>
     </nav>  
 </template>
+
+
+<script>
+
+export default {
+  name: 'navigation',
+  mounted(){
+    $(document).on('click', '#logOutButton', function(event) {
+      console.log("logged out")
+      localStorage.clear();
+      document.location.reload();
+      
+    }); 
+  } 
+} 
+
+</script>
