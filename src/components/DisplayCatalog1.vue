81641566db445e3be507704fd9cdd54ed279fe0f
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-06-16 22:11:48 +0800

    put placeholders for city images

--- src/components/DisplayCatalog1.vue
+++ src/components/DisplayCatalog1.vue
@@ -7,41 +7,14 @@
             <p class="subtitle text-primary">Stay and eat like a local</p>
             <h2>Our guides</h2>
           </div>
-          <div class="col-md-4 d-lg-flex align-items-center justify-content-end"><a href="category.html" class="text-muted text-sm">
-               
-              See all guides<i class="fas fa-angle-double-right ml-2"></i></a></div>
         </div>
         <div class="row">
           <swiper v-bind:options="swiperOption" class="guides-slider">
-              <swiper-slide class="h-auto px-2">
-                <div class="card card-poster gradient-overlay mb-4 mb-lg-0"><a href="category.html" class="tile-link"></a><img src="../assets/img/photo/new-york.jpg" alt="Card image" class="bg-image">
+              <swiper-slide class="h-auto px-2" v-for="city in cities" v-bind:key="city.name">
+                <div class="card card-poster gradient-overlay mb-4 mb-lg-0"><a href="category.html" class="tile-link"></a><img v-bind:src="city.image_url" v-bind:alt="city.name" class="bg-image">
                   <div class="card-body overlay-content">
-                    <h6 class="card-title text-shadow text-uppercase">New York</h6>
-                    <p class="card-text text-sm">The big apple</p>
-                  </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide class="h-auto px-2">
-                <div class="card card-poster gradient-overlay mb-4 mb-lg-0"><a href="category.html" class="tile-link"></a><img src="../assets/img/photo/paris.jpg" alt="Card image" class="bg-image">
-                  <div class="card-body overlay-content">
-                    <h6 class="card-title text-shadow text-uppercase">Paris</h6>
-                    <p class="card-text text-sm">Artist capital of Europe</p>
-                  </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide class="h-auto px-2">
-                <div class="card card-poster gradient-overlay mb-4 mb-lg-0"><a href="category.html" class="tile-link"></a><img src="../assets/img/photo/barcelona.jpg" alt="Card image" class="bg-image">
-                  <div class="card-body overlay-content">
-                    <h6 class="card-title text-shadow text-uppercase">Barcelona</h6>
-                    <p class="card-text text-sm">Dalí, Gaudí, Barrio Gotico</p>
-                  </div>
-                </div>
-              </swiper-slide>
-              <swiper-slide class="h-auto px-2">
-                <div class="card card-poster gradient-overlay mb-4 mb-lg-0"><a href="category.html" class="tile-link"></a><img src="../assets/img/photo/prague.jpg" alt="Card image" class="bg-image">
-                  <div class="card-body overlay-content">
-                    <h6 class="card-title text-shadow text-uppercase">Prague</h6>
-                    <p class="card-text text-sm">City of hundred towers</p>
+                    <h6 class="card-title text-shadow text-uppercase">{{city.name}}</h6>
+                    <p class="card-text text-sm">{{city.description}}</p>
                   </div>
                 </div>
               </swiper-slide>
@@ -57,10 +30,37 @@
     data() {
       return {
         swiperOption: {
-          slidesPerView: 7,
-          spaceBetween: 20,
+          slidesPerView: 6,
+          spaceBetween: 15,
           loop: true
-        }
+        },
+        cities: [
+          {
+            "name" : "Boracay",
+            "image_url" : "https://via.placeholder.com/600X900",
+            "description" : "Boracay is .." 
+          },
+          {
+            "name" : "Baguio",
+            "image_url" : "https://via.placeholder.com/600X900",
+            "description" : "Baguio is .." 
+          },
+          {
+            "name" : "Coron",
+            "image_url" : "https://via.placeholder.com/600X900",
+            "description" : "Coron is .." 
+          },
+          {
+            "name" : "Tagaytay",
+            "image_url" : "https://via.placeholder.com/600X900",
+            "description" : "Tagaytay is .." 
+          },
+          {
+            "name" : "La Union",
+            "image_url" : "https://via.placeholder.com/600X900",
+            "description" : "La Union is .." 
+          }
+        ]
       }
     }
   }
