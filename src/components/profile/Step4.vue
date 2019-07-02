5b353d1e23b6641328fb0d4720c57a0cf8a73fd7
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-07-02 22:38:22 +0800

    modify user success update

---
+++ src/components/profile/Step4.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="profile-step4">
+	    <div style="height: 8px; top: 71px;" class="progress rounded-0 sticky-top">
+	      <div role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" class="progress-bar"></div>
+	    </div>
+	    <section class="py-5 py-lg-6">
+	      <div class="container text-center">
+	        <p class="subtitle text-primary">Success!</p>
+	        <h1 class="h2 mb-5">Your Profile has been updated!</h1>
+	        <p class="mb-5"><img src="../../assets/img/illustration/undraw_celebration_0jvk.svg" alt="" style="width: 400px;" class="img-fluid"></p>
+	        <p class="text-muted mb-5">Thank you, Ondrej. Your profile has been updated!</p>
+	        <p class="mb-5"><a href="#" class="btn btn-primary mr-2 mb-2">View Profile</a><a href="#" class="btn btn-outline-muted mb-2">Home</a></p>
+	      </div>
+	    </section>
+	</div>
+</template>
