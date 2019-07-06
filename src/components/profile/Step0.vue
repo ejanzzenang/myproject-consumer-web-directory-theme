d63af14e0fd10d89f57e01a1902439de1fd2168d
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-07-06 18:34:07 +0800

    Add simple validation and form submit

--- src/components/profile/Step0.vue
+++ src/components/profile/Step0.vue
@@ -5,29 +5,32 @@
 	    </div>
 		<section class="py-5 py-lg-7">
 	      <div class="container">
-	        <div class="row">
-	          <div class="col-lg-5">
-	            <p class="subtitle text-primary">COMPLETE YOUR PROFILE</p>
-	            <h1 class="h2 mb-5">Let's get started!</h1>
-	            <p class="text-muted">Are you a Philippine Resident?</p>
-              	<div class="row">
-                <div class="col-md-12">
-                  <div class="form-group">
-                  <div class="custom-control custom-radio">
-                    <input type="radio" id="locality_0" name="locality" class="custom-control-input">
-                    <label for="locality_0" class="custom-control-label">I am a Philippine Resident</label>
-                  </div>
-                  <div class="custom-control custom-radio">
-                    <input type="radio" id="locality_1" name="locality" class="custom-control-input">
-                    <label for="locality_1" class="custom-control-label">I am a Non-Philippine Resident</label>
-                  </div>
-                  </div>
-                </div>
-              </div>
-	            <p class="mb-5 mb-lg-0"><router-link to="step1"><button class="btn btn-primary px-3">Next step<i class="fa-chevron-right fa ml-2"></i></button></router-link></p>
-	          </div>
-	          <div class="col-lg-5 ml-auto d-flex align-items-center"><img src="../../assets/img/illustration/undraw_celebration_0jvk.svg" alt="" style="width: 400px;" class="img-fluid"></div>
-	        </div>
+       		<form id="residency" action='/profile/step1'>
+		        <div class="row">
+		          <div class="col-lg-5">
+		            <p class="subtitle text-primary">COMPLETE YOUR PROFILE</p>
+		            <h1 class="h2 mb-5">Let's get started!</h1>
+		            <p class="text-muted">Are you a Philippine Resident?</p>
+	              	<div class="row">
+	                <div class="col-md-12">
+	                  <div class="form-group">
+	                  <div class="custom-control custom-radio">
+	                    <input type="radio" id="residency_0" name="locality" class="custom-control-input" required>
+	                    <label for="residency_0" class="custom-control-label">I am a Philippine Resident</label>
+	                  </div>
+	                  <div class="custom-control custom-radio">
+	                    <input type="radio" id="residency_1" name="locality" class="custom-control-input required">
+	                    <label for="residency_1" class="custom-control-label">I am a Non-Philippine Resident</label>
+	                  </div>
+
+	                  </div>
+	                </div>
+	              </div>
+		            <p class="mb-5 mb-lg-0"><button type='submit' class="btn btn-primary px-3">Next step<i class="fa-chevron-right fa ml-2"></i></button></p>
+		          </div>
+		          <div class="col-lg-5 ml-auto d-flex align-items-center"><img src="../../assets/img/illustration/undraw_celebration_0jvk.svg" alt="" style="width: 400px;" class="img-fluid"></div>
+		        </div>
+	    	</form>
 	      </div>
     </section>
 	</div>
