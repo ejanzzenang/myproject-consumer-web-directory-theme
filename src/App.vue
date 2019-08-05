c8cc622ab607ff6e3c3494fefa6adfe2764f3f57
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-08-05 23:04:34 +0800

    move Initialize storage

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@
   // @ is an alias to /src
 import Navigation from '@/components/Navigation.vue'
 import Footer from '@/components/Footer.vue'
+import {CognitoAuth} from 'amazon-cognito-auth-js';
+var cognitoUserPoolId = 'ap-southeast-1_AQoxu5EIr'; 
+var cognitoUserPoolClientId = '19mgjrlikq9nljgcfjo0k1ajja'; 
+var appclient_id  = '19mgjrlikq9nljgcfjo0k1ajja'
+var awsRegion = 'ap-southeast-1';
 
 export default {
   name: 'home',
@@ -20,8 +25,44 @@ export default {
     Navigation,
     Footer
   },
-  created(){
-     function injectSvgSprite(path) {
+  methods:{
+    initializeStorage() {
+        var identityPoolId = cognitoUserPoolId;
+        var userPoolId = cognitoUserPoolId;
+        var clientId = cognitoUserPoolClientId;
+        var loginPrefix = 'cognito-idp.' + awsRegion + '.amazonaws.com/' + identityPoolId;
+
+        localStorage.setItem('identityPoolId', identityPoolId);
+        localStorage.setItem('userPoolId', userPoolId);
+        localStorage.setItem('clientId', clientId);
+        localStorage.setItem('loginPrefix', loginPrefix);
+    },
+    initCognitoSDK: function() {
+          var authData = {
+              ClientId : appclient_id, 
+              AppWebDomain : 'app-hetchly.auth.ap-southeast-1.amazoncognito.com',
+              TokenScopesArray : ['email'], 
+              RedirectUriSignIn : 'http://localhost:8080',
+              RedirectUriSignOut : 'http://localhost:8080',
+              IdentityProvider : 'Facebook', 
+              UserPoolId : cognitoUserPoolId, 
+            };
+
+            var auth = new CognitoAuth(authData)
+            
+            auth.userhandler = {
+              // * E.g.
+              onSuccess: function(result) {
+                alert("Sign in success");
+              },
+              onFailure: function(err) {
+                alert("Error!" + err);
+              }          
+            }
+
+            return auth
+    },
+    injectSvgSprite(path) {
           var ajax = new XMLHttpRequest();
           ajax.open("GET", path, true);
           ajax.send();
@@ -31,13 +72,31 @@ export default {
             div.innerHTML = ajax.responseText;
             document.body.insertBefore(div, document.body.childNodes[0]);
           }
-      }    
-      // to avoid CORS issues when viewing using file:// protocol, using the demo URL for the SVG sprite
-      // use your own URL in production, please :)
-      // https://demo.bootstrapious.com/directory/1-0/icons/orion-svg-sprite.svg
-      //- injectSvgSprite('icons/orion-svg-sprite.svg'); 
-      injectSvgSprite('https://demo.bootstrapious.com/directory/1-1/icons/orion-svg-sprite.svg'); 
+    }     
+  },
+  computed:{
+  
+  },
+  created(){
+    this.injectSvgSprite('https://demo.bootstrapious.com/directory/1-1/icons/orion-svg-sprite.svg');
+
+    // initialize login storage
+    this.initializeStorage()
+    console.log("Initialized local storage in App.vue")
+
+    // this.$store.state.auth = this.initCognitoSDK()
+
+    // var curUrl = window.location.href;
+    // auth.parseCognitoWebResponse(curUrl);
+    // initialize auth object for login with facebook
+    // var auth = this.initCognitoSDK()
+    // console.log("Initialized cognitoSDK in App.vue")
+    // console.log(auth)
+  },
+  mounted(){
+
   }
+
 }
 
 </script>
