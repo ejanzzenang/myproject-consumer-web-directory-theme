55a7541a905534372b762d3cb4f6c82525fc00d0
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-08-18 21:19:08 +0800

    add Services to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <SearchBar/>
+    <Services/>
     <DisplayCatalog1/>
     <DisplayProducts/>
     <PhotoWheel/> 
@@ -10,6 +11,7 @@
 <script>
 // @ is an alias to /src
 import SearchBar from '@/components/SearchBar.vue'
+import Services from '@/components/Services.vue'
 import DisplayCatalog1 from '@/components/DisplayCatalog1.vue'
 import DisplayProducts from '@/components/products/DisplayProduct.vue'
 import PhotoWheel from '@/components/PhotoWheel.vue'
@@ -18,6 +20,7 @@ export default {
   name: 'home',
   components: {
     SearchBar, 
+    Services,
     DisplayCatalog1,
     DisplayProducts,
     PhotoWheel
